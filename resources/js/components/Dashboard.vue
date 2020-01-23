27b03c9bf3868a62ed83619d8b077e63a5237db4
K. Usman <kusman758@gmail.com>
2020-01-23 22:45:13 +0800

    wew

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -8,7 +8,7 @@
                       <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
 
                       <div class="info-box-content">
-                        <span class="info-box-text">Patient</span>
+                        <span class="info-box-text">Patients</span>
                         <span class="info-box-number">
                             
                           {{patients.length}}
